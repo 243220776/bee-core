dcfa6cbb56d8f3e76094ba99cc355ae16a307ad7
zhangyaxin <dayizyx888>
2021-09-24 18:06:20 +0800

    调整

--- src/main/java/com/yestae/bee/boot/BeeStarter.java
+++ src/main/java/com/yestae/bee/boot/BeeStarter.java
@@ -1,15 +1,53 @@
 package com.yestae.bee.boot;
 
+import com.yestae.bee.config.BeeClientConfiguration;
+import com.yestae.bee.config.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 @ComponentScan({ "com.yestae", "${bee.config.scanPackage}" })
 public class BeeStarter {
+    private static final Logger logger = LoggerFactory.getLogger(BeeStarter.class);
 
-    public static ConfigurableApplicationContext run(final Class<?> clas, String args[]) {
-        return SpringApplication.run(clas, args);
+    public static void run(final Class<?> resrouce, String args[]) {
+        setDefaultProperty();
+        runSpring(resrouce, args);
+    }
+
+    private static void runSpring(final Class<?> resrouce, String args[]) {
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(resrouce);
+        SpringApplication app = builder.build();
+        final ConfigurableApplicationContext appContext = app.run(args);
+        //jvm关闭时 优雅推出spring
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+
+            public void run() {
+                synchronized (resrouce) {
+                    SpringApplication.exit(appContext);
+                    logger.info("SpringApplication exited...");
+                    resrouce.notify();
+                }
+            }
+        });
+        synchronized (resrouce) {
+            while (true) {
+                try {
+                    resrouce.wait();
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+
+    private static void setDefaultProperty() {
+//        System.setProperty("spring.cloud.config.enabled", "false");
+        System.setProperty(Constants.CONFIG_APPNAME_KEY, BeeClientConfiguration.getLocalProperies().getAppName());
     }
 }
--- src/main/java/com/yestae/bee/config/Constants.java
+++ src/main/java/com/yestae/bee/config/Constants.java
@@ -8,9 +8,9 @@ package com.yestae.bee.config;
  */
 public class Constants {
 
-	public static final String CONFIG_HTTP_PORT = "server.port";
+	public static final String CONFIG_HTTP_PORT = "bee.httpPort";
 
-	public static final String CONFIG_APPNAME_KEY = "spring.application.name";
+	public static final String CONFIG_APPNAME_KEY = "bee.application.name";
 
 	public static final String CONFIG_SCANPACKAGE_KEY = "bee.config.scanPackage";
 
